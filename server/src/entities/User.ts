776912a3f22eb746c2bf8e4218a861a772c6c6ff
anhtudo97 <anhtudo97@gmail.com>
2021-08-28 15:59:25 +0700

    feat: add resolver graphql

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,11 +1,14 @@
-import { Field } from 'type-graphql';
+import { Field, ID, ObjectType } from 'type-graphql';
 import {BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm'
 
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+    @Field(_type => ID)
     @PrimaryGeneratedColumn()
     id!: number;
 
+    @Field()
     @Column({unique: true})
     username!: string;
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,15 @@
-import { Post } from './entities/Post';
+import { UserResolver } from './resolvers/user';
+import { Post } from "./entities/Post";
 require("dotenv").config();
 
 import "reflect-metadata";
 import express from "express";
 import { createConnection } from "typeorm";
 import { User } from "./entities/User";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 const main = async () => {
   await createConnection({
@@ -14,11 +19,29 @@ const main = async () => {
     password: process.env.POSTGRES_PASSWORD_DEV,
     logging: true,
     synchronize: true,
-    entities: [User, Post]
+    entities: [User, Post],
   });
   const app = express();
 
-  app.listen(4000, () => console.log("DB is listening on port 4000"));
+  // Apollo server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, UserResolver],
+      validate: false,
+    }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () =>
+    console.log(
+      `Server is started on port ${PORT}. GraphQL Server started on localhost: ${apolloServer.graphqlPath}`
+    )
+  );
 };
 
 main().catch((error) => console.log(error));
