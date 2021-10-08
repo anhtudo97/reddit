72fa3bb3b8ed152da405797e05ae1c4efd6c75f2
anhtudo97 <anhtudo97@gmail.com>
2021-10-08 22:58:12 +0700

    chore: update entities

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -8,7 +8,9 @@ import {
   UpdateDateColumn,
   Column,
   ManyToOne,
+  OneToMany,
 } from "typeorm";
+import { Upvote } from "./Upvote";
 @ObjectType()
 @Entity()
 export class Post extends BaseEntity {
@@ -27,6 +29,16 @@ export class Post extends BaseEntity {
   @Field((_type) => User)
   @ManyToOne(() => User, (user) => user.posts)
   user: User;
+
+  @OneToMany((_to) => Upvote, (upvote) => upvote.post)
+  upvotes: Upvote[];
+
+  @Field()
+  @Column({ default: 0 })
+  points!: number;
+
+  @Field()
+  voteType!: number;
 
   @Field()
   @Column()
